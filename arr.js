6c4f02314263982ee7eb8edccba65c94cd649c2d
Abdelrahman-Osama <abdelrahmanosama620@gmail.com>
2022-08-29 19:33:51 +0200

    fc

--- arr.js
+++ arr.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000','https://abdelrahmanosama62.github.io']
 }));
 var arr = [
   {
